8f76c4ec0ab3212f9acdc2f2a424cb4d9dc8c0ec
jshanxuerui <764795838@qq.com>
2020-05-22 11:16:41 +0800

    test

---
+++ test01/src/test/Test05.java
@@ -0,0 +1,12 @@
+package test;
+
+/**
+ * @ClassName Test05
+ * @Author:单雪瑞
+ * @Date 2020/5/22 11:12
+ */
+public class Test05 {
+    public static void main(String[] args) {
+        System.out.println(060606);
+    }
+}
